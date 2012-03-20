c649a2f9088bbf32cdc7720e7a4498c87020ae9a
Rory McKinley <rorymckinley@gmail.com>
2012-03-20 10:51:10 +0200

    Refactor Profiler

--- lib/are_we_there_yet/profiler.rb
+++ lib/are_we_there_yet/profiler.rb
@@ -5,34 +5,37 @@ module AreWeThereYet
     end
 
     def list_files
-      sort_file_averages average_file_execution_times
+      sorted_output(average_file_execution_times)
     end
 
     def list_examples(file_path)
       file = SpecFile.first(:path => file_path)
       example_averages_for_sorting = file.examples.map { |ex| { :example => ex.to_s, :average_execution_time => ex.average_time } }
 
-      example_averages_for_sorting.sort { |x,y| y[:average_execution_time] <=> x[:average_execution_time] }
+      sorted_output(example_averages_for_sorting)
     end
+
     private
 
     def average_file_execution_times
       example_averages = Example.all.map { |ex| { :file => ex.spec_file.to_s, :average_execution_time => ex.average_time } }
 
-      example_averages.inject({}) do |output, average_hash|
+      av_by_file = example_averages.inject({}) do |output, average_hash|
         output[average_hash[:file]] ||= 0.0
         output[average_hash[:file]] += average_hash[:average_execution_time]
         output
       end
-    end
 
-    def sort_file_averages(file_averages)
-      file_averages_for_sorting = average_file_execution_times.inject([]) do |output,file_time|
+      file_averages_for_sorting = av_by_file.inject([]) do |output,file_time|
         output << { :file => file_time.first, :average_execution_time => file_time.last }
         output
       end
 
-      file_averages_for_sorting.sort { |x,y| y[:average_execution_time] <=> x[:average_execution_time] }
+      file_averages_for_sorting
+    end
+
+    def sorted_output(data_to_sort)
+      data_to_sort.sort { |x,y| y[:average_execution_time] <=> x[:average_execution_time] }
     end
   end
 end
