5a62b62ff54788ff820bde80225ff48b7526ad10
Rory McKinley <rorymckinley@gmail.com>
2012-04-07 19:20:12 +0200

    Part 1 - covering tests for DM functionality

--- spec/lib/spec_file_spec.rb
+++ spec/lib/spec_file_spec.rb
@@ -4,39 +4,37 @@ describe AreWeThereYet::SpecFile do
   before(:each) do
     @db_name = "/tmp/are_we_there_yet_#{Time.now.to_i}_#{rand(100000000)}_spec.sqlite"
     DataMapper.setup(:default, "sqlite://#{@db_name}")
+
+    metric_sets = { :runs => [
+      [
+        { :location => "/path/to/spec", :description => "blaah", :execution_time => 10 },
+        { :location => "/path/to/spec", :description => "blaah_some_more", :execution_time => 5 },
+        { :location => "/path/to/other/spec", :description => "asdfghij", :execution_time => "5" }
+      ],
+    ]}
+    MetricFactory.new(@db_name).add_metrics(metric_sets)
+
+    @f = AreWeThereYet::SpecFile.for_path("/path/to/spec")
   end
 
   after(:each) do
     File.unlink(@db_name) if File.exists? @db_name
   end
 
+  it "exposes the id generated for the file" do
+    @f.id.should == 1
+  end
+
   it "uses the file path as the string representation of a file" do
-    f = AreWeThereYet::SpecFile.new
-    f.path = "/path/to/spec"
-    f.to_s.should eq f.path
+    @f.to_s.should eq @f.path
   end
 
   it "retrieves an instance of itself based on the file path" do
-    metric_sets = { :runs => [
-      [
-        { :location => "/path/to/spec", :description => "blaah", :execution_time => 10 },
-        { :location => "/path/to/spec", :description => "blaah_some_more", :execution_time => 5 },
-      ],
-    ]}
-    MetricFactory.new(@db_name).add_metrics(metric_sets)
-
-    f = AreWeThereYet::SpecFile.for_path("/path/to/spec")
-    f.path.should eql "/path/to/spec"
+    @f.path.should eql "/path/to/spec"
   end
 
-  it "maintains a collection of all its examples" do
-    pending
-    metric_sets = { :runs => [
-      [
-        { :location => "/path/to/spec", :description => "blaah", :execution_time => 10 },
-        { :location => "/path/to/spec", :description => "blaah_some_more", :execution_time => 5 },
-      ],
-    ]}
-    MetricFactory.new(@db_name).add_metrics(metric_sets)
+  it "maintains a collection of all its associated examples" do
+    @f.examples.size.should == 2
+    (@f.examples.all? { |ex| ["blaah", "blaah_some_more"].include? ex.description }).should be_true
   end
 end
