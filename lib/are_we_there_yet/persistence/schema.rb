bdbf88be7f44db78f6a598ff20d88aeb24085031
Rory McKinley <rorymckinley@gmail.com>
2012-04-18 22:23:24 +0200

    Schema creation reworked - next step refactor

--- lib/are_we_there_yet/persistence/schema.rb
+++ lib/are_we_there_yet/persistence/schema.rb
@@ -2,9 +2,32 @@ module AreWeThereYet
   module Persistence
     module Schema
       def self.create(connection)
-        connection.create_table(:spec_files) do
-          primary_key :id
-          DateTime :started_at
+        if connection.tables.empty?
+          connection.transaction do
+            connection.create_table(:spec_files) do
+              primary_key :id
+              String :path
+              index :path
+            end
+            connection.create_table(:runs) do
+              primary_key :id
+              DateTime :started_at
+              DateTime :ended_at
+            end
+            connection.create_table(:examples) do
+              primary_key :id
+              Integer :spec_file_id
+              column :description, :text
+              index [:spec_file_id, :description]
+            end
+            connection.create_table(:metrics) do
+              primary_key :id
+              Integer :example_id
+              Float :execution_time
+              DateTime :created_at
+              Integer :run_id
+            end
+          end
         end
       end
     end
--- spec/lib/persistence/schema_spec.rb
+++ spec/lib/persistence/schema_spec.rb
@@ -41,11 +41,72 @@ describe AreWeThereYet::Persistence::Schema do
     @connection = Sequel.connect(@db)
   end
 
-  it "should create a table for storing spec files" do
+  it "creates a table for storing spec files" do
     AreWeThereYet::Persistence::Schema.create(@connection)
 
-    @connection.tables.should include :spec_files
-    @connection.schema(:spec_files).should have_field :field => :id, :attributes => {:type => :integer, :primary_key => true}
-    @connection.schema(:spec_files).should have_field :field => :started_at, :attributes => { :type => :datetime}
+    schema = @connection.schema(:spec_files)
+
+    schema.should have_field :field => :id, :attributes => {:type => :integer, :primary_key => true}
+    schema.should have_field :field => :path, :attributes => {:type => :string}
+
+    @connection.indexes(:spec_files).should == { :spec_files_path_index => {:unique=>false, :columns=>[:path]}}
+  end
+
+  it "creates a table for tracking spec runs" do
+    AreWeThereYet::Persistence::Schema.create(@connection)
+
+    schema = @connection.schema(:runs)
+    schema.should have_field :field => :id, :attributes => {:type => :integer, :primary_key => true}
+    schema.should have_field :field => :started_at, :attributes => {:type => :datetime}
+    schema.should have_field :field => :ended_at, :attributes => {:type => :datetime}
+  end
+
+  it "creates a table for tracking examples" do
+    AreWeThereYet::Persistence::Schema.create(@connection)
+
+    schema = @connection.schema(:examples)
+    schema.should have_field :field => :id, :attributes => {:type => :integer, :primary_key => true}
+    schema.should have_field :field => :spec_file_id, :attributes => {:type => :integer}
+    schema.should have_field :field => :description, :attributes => {:type => :string}
+
+    @connection.indexes(:examples).should == {
+      :examples_spec_file_id_description_index => {:unique=>false, :columns=>[:spec_file_id, :description]}
+    }
+  end
+
+  it "creates a table for tracking metrics" do
+    AreWeThereYet::Persistence::Schema.create(@connection)
+
+    schema = @connection.schema(:metrics)
+    schema.should have_field :field => :id, :attributes => {:type => :integer, :primary_key => true}
+    schema.should have_field :field => :example_id, :attributes => {:type => :integer}
+    schema.should have_field :field => :execution_time, :attributes => {:type => :float}
+    schema.should have_field :field => :created_at, :attributes => {:type => :datetime}
+    schema.should have_field :field => :run_id, :attributes => {:type => :integer}
+  end
+
+  it "rolls back table creation if there is a problem creating any one of the tables" do
+    broken_connection = mock(Sequel::SQLite::Database, :tables => [])
+    broken_connection.stub(:create_table) do |arg, bl|
+      if arg == :metrics
+        raise RuntimeError
+      else
+        @connection.create_table(arg, &bl)
+      end
+    end
+
+    expect { AreWeThereYet::Persistence::Schema.create(broken_connection) }.should raise_error
+
+    @connection.tables.should be_empty
+  end
+
+  it "does not create tables if there already tables present in the database" do
+    @connection.create_table(:dummy) do
+      primary_key :id
+    end
+
+    AreWeThereYet::Persistence::Schema.create(@connection)
+
+    @connection.tables.should == [:dummy]
   end
 end
