42ad00e3255c43ffe1a80195c486067d0c149b4e
Rory McKinley <rorymckinley@gmail.com>
2012-04-21 22:27:14 +0200

    DRYed up table creation

--- lib/are_we_there_yet/persistence/schema.rb
+++ lib/are_we_there_yet/persistence/schema.rb
@@ -1,31 +1,36 @@
 module AreWeThereYet
   module Persistence
     module Schema
+      @@tables = {
+        :spec_files => Proc.new {
+          primary_key :id
+          String :path
+          index :path
+        },
+        :runs => Proc.new {
+          primary_key :id
+          DateTime :started_at
+          DateTime :ended_at
+        },
+        :examples => Proc.new {
+          primary_key :id
+          Integer :spec_file_id
+          column :description, :text
+          index [:spec_file_id, :description]
+        },
+        :metrics => Proc.new {
+          primary_key :id
+          Integer :example_id
+          Float :execution_time
+          DateTime :created_at
+          Integer :run_id
+        }
+      }
       def self.create(connection)
         if connection.tables.empty?
           connection.transaction do
-            connection.create_table(:spec_files) do
-              primary_key :id
-              String :path
-              index :path
-            end
-            connection.create_table(:runs) do
-              primary_key :id
-              DateTime :started_at
-              DateTime :ended_at
-            end
-            connection.create_table(:examples) do
-              primary_key :id
-              Integer :spec_file_id
-              column :description, :text
-              index [:spec_file_id, :description]
-            end
-            connection.create_table(:metrics) do
-              primary_key :id
-              Integer :example_id
-              Float :execution_time
-              DateTime :created_at
-              Integer :run_id
+            @@tables.each do |name,attributes|
+              connection.create_table(name, &attributes)
             end
           end
         end
