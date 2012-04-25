712920037e8423f095864ca37c925109545dcdaf
Rory McKinley <rorymckinley@gmail.com>
2012-04-25 11:47:19 +0200

    Refactor Profiler

--- lib/are_we_there_yet/profiler.rb
+++ lib/are_we_there_yet/profiler.rb
@@ -5,54 +5,24 @@ module AreWeThereYet
     end
 
     def list_files
-      sorted_output(average_file_execution_times)
+      averages_by_file = get_average_per_descriptor(metrics_by_file)
+
+      sorted_output(transform_averages_for_sorting(averages_by_file, :file))
     end
 
     def list_examples(file_path)
-      metrics = Metric.all(@db)
-
-      metrics_by_file = metrics.group_by { |m| m.path }
-
       if metrics_by_file[file_path]
-
         metrics_by_example = metrics_by_file[file_path].group_by { |m| m.description }
 
-        metrics_by_example_per_run = metrics_by_example.merge(metrics_by_example) do |example,metrics,metrics|
-          metrics.group_by { |m| m.run_id }
-        end
+        averages_by_example = get_average_per_descriptor(metrics_by_example)
 
-        averages_by_example = metrics_by_example_per_run.merge(metrics_by_example_per_run) do |key, runs, runs|
-          find_average_time_for runs
-        end
-
-        example_averages_for_sorting = averages_by_example.inject([]) do |output, (example, average_execution_time)|
-          output << { :example => example, :average_execution_time => average_execution_time }
-        end
-
-        sorted_output(example_averages_for_sorting)
+        sorted_output(transform_averages_for_sorting(averages_by_example, :example))
       else
         []
       end
     end
 
     private
-
-    def average_file_execution_times
-      metrics = Metric.all(@db)
-
-      metrics_by_file = metrics.group_by { |m| m.path }
-
-      metrics_by_file_per_run = metrics_by_file.merge(metrics_by_file) do |file, metrics, metrics|
-        metrics.group_by { |m| m.run_id }
-      end
-
-      # The next line is really just a map operation
-      averages_by_file = metrics_by_file_per_run.merge(metrics_by_file_per_run) { |key, runs, runs| find_average_time_for runs }
-
-      averages_by_file.inject([]) do |output, (file_path, average_execution_time)|
-        output << { :file => file_path, :average_execution_time => average_execution_time }
-      end
-    end
 
     def sorted_output(data_to_sort)
       data_to_sort.sort { |x,y| y[:average_execution_time] <=> x[:average_execution_time] }
@@ -64,6 +34,26 @@ module AreWeThereYet
       end
 
       (total_per_run.inject(:+))/total_per_run.size
+    end
+
+    def metrics_by_file
+      @metrics_by_file || Metric.all(@db).group_by { |m| m.path }
+    end
+
+    def get_average_per_descriptor(metric_set)
+      metrics_by_descriptor_per_run = metric_set.merge(metric_set) do |descriptor,metrics,metrics|
+        metrics.group_by { |m| m.run_id }
+      end
+
+      averages_by_descriptor = metrics_by_descriptor_per_run.merge(metrics_by_descriptor_per_run) do |descriptor, runs, runs|
+        find_average_time_for runs
+      end
+    end
+
+    def transform_averages_for_sorting(averages_by_descriptor, descriptor_name)
+      averages_by_descriptor.inject([]) do |output, (descriptor, average_execution_time)|
+        output << { descriptor_name.to_sym => descriptor, :average_execution_time => average_execution_time }
+      end
     end
   end
 end
